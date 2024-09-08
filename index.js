3c938a6ec195a8073e91bac03c1056b618077dfb
mahan nezafat <nezafatmahan@gmail.com>
2024-09-08 20:27:35 +0330

    setup basic security(helmet)/ logging(morgan)

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 require("dotenv/config");
+var morgan = require("morgan");
+var helmet_1 = require("helmet");
 var app = express();
 var port = process.env.PORT;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((0, helmet_1.default)());
+app.get('/', function (req, res) {
+    return res.status(200).json({
+        success: true,
+        message: 'response ok'
+    });
+});
 app.listen(port, function () { return console.log("server running on port ".concat(port)); });
--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import express = require('express');
 import 'dotenv/config'
+import morgan = require('morgan')
+import helmet from 'helmet'
 const app = express();
 const port = process.env.PORT
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'))
+app.use(helmet())
 
-
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: 'response ok'
+    })
+})
 
 
 
